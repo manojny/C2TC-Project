e68d79e79feaebc321e26e098644cb0aa9bbc6a5
User <User@DESKTOP-SCPS259>
2022-08-03 12:13:48 +0530

    First Commit

---
+++ DemoProject/src/com/cg/demoproject/DemoClass.java
@@ -0,0 +1,11 @@
+package com.cg.demoproject;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+	System.out.println("hello world");
+    System.out.println("welcome to java programming");
+
+	}
+
+}
